f3c614d778193754473ca343fbe8ac02c9de149d
Meliqe <55erebor55@gmail.com>
2024-12-29 18:32:54 +0300

    ilgili kategoriye tıklanıldığında o türdeki filmler geliyor ama html css düzenlemesi şimdilik yok.

--- src/app/pages/category-details/category-details.component.ts
+++ src/app/pages/category-details/category-details.component.ts
@@ -14,16 +14,27 @@ import {NgForOf} from '@angular/common';
 })
 export class CategoryDetailsComponent {
   categories: any;
+  PathPrefix: string = 'data:image/jpeg;base64,';
+  categoryName: string = ''; // Seçilen kategori adını tutar
 
   constructor(private router: Router, private filmService: FilmService,private route: ActivatedRoute) { }
   ngOnInit(): void {
     this.getFilmsByCategoryName();
+    this.getCategoryName();
   }
-
+  getCategoryName(): void {
+    this.categoryName = this.route.snapshot.paramMap.get('categoryName') || 'Kategori Adı';
+  }
   getFilmsByCategoryName():void{
     const categories =history.state.filmsbycategory;
     if(categories){
       this.categories = categories;
+      console.log(this.categories);
+      this.categories.forEach((film: any) => {
+        if (film.posterPath) {
+          film.posterPath = `${this.PathPrefix}${film.posterPath}`;
+        }
+      });
     }
   }
 }

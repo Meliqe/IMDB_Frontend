4d84a9b3043da316edc76376c9089474a2c8c349
Meliqe <55erebor55@gmail.com>
2025-01-01 23:08:26 +0300

    kullanıcı profil sayfası için storagede saklanan veriler çekildi userdetails componentine ve basit bir html kodu yazıldı. kullanıcı bilgileri gözüküyor.

--- src/app/pages/user-details/user-details.component.ts
+++ src/app/pages/user-details/user-details.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import {AuthService} from '../../services/auth.service';
+import {Router} from '@angular/router';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-user-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule], //ng direktiflerini kullanmak için
   templateUrl: './user-details.component.html',
   styleUrl: './user-details.component.css'
 })
 export class UserDetailsComponent {
-constructor(private authService:AuthService) {
+  currentUser: any =null;
+constructor(private authService:AuthService , private router:Router) {
 }
+ngOnInit():void{
+  this.currentUser=this.authService.getCurrentUser();
+  if (!this.currentUser) {
+    this.router.navigate(['/login']);
+  }
+}
+
 
 }

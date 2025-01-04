5da665e10171f7898a2b7217d6246f8143839e33
Meliqe <55erebor55@gmail.com>
2025-01-04 15:23:29 +0300

    update user endpointi interceptora verildi, artık token kontrolü yapılacak editleme yapılırken.

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -5,7 +5,7 @@ import {inject} from '@angular/core';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
   const authService = inject(AuthService); // o yüzden bu şekilde enjecte etmeliyiz
-  const protectedEndpoints = ['/userdetails','/addcomment'];
+  const protectedEndpoints = ['/userdetails','/addcomment','updateuser'];
 
   //eğer istek korunan endpoinete yapılmıyorsa direkt gönderiyoruz
   //bu korunan endpointe istek yapmıyorsak

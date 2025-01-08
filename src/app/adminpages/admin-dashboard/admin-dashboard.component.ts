bdd132fcebeb1523a4e5287ee5f4322ec543f1a5
Meliqe <55erebor55@gmail.com>
2025-01-08 16:54:36 +0300

    admin giriş yaptığında filmservis metodundan getallfilmsi çağırıyorum ve kendi oluşturduğum film dizine atıyorum, bunu consolde görmek için log attım , başarılı bir şekilde geliyor console a.

--- src/app/adminpages/admin-dashboard/admin-dashboard.component.ts
+++ src/app/adminpages/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {AdminService} from '../../services/admin.service';
+import {Router} from '@angular/router';
+import {FilmService} from '../../services/film.service';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -8,5 +11,21 @@ import { Component } from '@angular/core';
   styleUrl: './admin-dashboard.component.css'
 })
 export class AdminDashboardComponent {
+
+  films: any = [];
+
+  constructor(private adminService: AdminService, private router: Router,private filmService: FilmService) {}
+  ngOnInit() {
+    this.filmService.getAllFilms().subscribe({
+      next: data => {
+        this.films = data;
+        console.log(this.films);
+      },
+      error: err => {
+       console.log("Bilgiler yüklenmedi",err);
+      }
+    })
+  }
+
 
 }
